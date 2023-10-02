52a10843889592dee1561ce7a64670a0df9441a1
Matt Wimer (Home) <mattwimer1@gmail.com>
2023-10-01 17:19:49 -0700

    Implemented the pseudocode.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random as r
+import itertools # standard library containing permutations function
 
 size = int(input("Enter number of cities to generate: "))
 # this week was busy but I would have liked to try using pandas to implement this table
@@ -17,6 +18,37 @@ for row in range(size):
         print(sample[row][col],end=" ")
     print()
 
-naiveTSP(sample)
+def totalDistance(origin, route, mat):
+    total = 0
+    current = origin
+
+    for next in route:
+        total += mat[current][next]
+        current = next
+    
+    return total + mat[current][origin]
+
+
+
 def naiveTSP(mat):
-    pass
+    n = len(mat)
+    origin = 0
+    otherCities = [x for x in range(1,n)]
+    
+    minDistance = float('inf')
+    bestRoute = []
+
+    for route in itertools.permutations(otherCities):
+        currentDistance = totalDistance(origin, route, mat)
+        
+        if currentDistance < minDistance:
+            minDistance = currentDistance
+            bestRoute = list(route)
+            bestRoute.insert(origin, 0)
+    
+    return bestRoute
+    
+
+
+best = naiveTSP(sample) # test function
+print(f"The best route is {best}, which costs ${totalDistance(0, best, sample)-sample[best[-1]][0]}")
